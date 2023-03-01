bcffbf46bf1e61ebe65b978c4c30fe9e414c714a
Daniel Ho <qho@adobe.com>
2023-03-01 11:49:15 -0800

    Add more styling for small screens

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { readBlockConfig, decorateIcons, decorateButtons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
@@ -11,9 +11,24 @@ export default async function decorate(block) {
 
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
-  const html = await resp.text();
-  const footer = document.createElement('div');
-  footer.innerHTML = html;
-  await decorateIcons(footer);
-  block.append(footer);
+  if (resp.ok) {
+    const html = await resp.text();
+    const footer = document.createElement('div');
+    footer.innerHTML = html;
+
+    const classes = ['logo', 'text', 'buttons', 'links', 'social', 'legal', 'disclaimer'];
+    classes.forEach((c, i) => {
+      const section = footer.children[i];
+      if (section) section.classList.add(`footer-${c}`);
+    });
+
+    const legalClasses = ['legal', 'privacy', 'security'];
+    await decorateIcons(footer);
+    await decorateButtons(footer.querySelector('.footer-buttons'));
+    legalClasses.forEach((c, i) => {
+      const section = footer.querySelector('.footer-legal').children[i];
+      if (section) section.classList.add(`${c}`);
+    });
+    block.append(footer);
+  }
 }
