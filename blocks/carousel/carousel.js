a00d43eca73e9dca481ba505627b6acb20f4d890
Sean Steimer <ssteimer@adobe.com>
2023-03-04 21:14:34 -0800

    fix: all the carousel things

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,9 +1,26 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
 import { createElement } from '../../scripts/scripts.js';
+
+function changeSlide(direction, block) {
+  const active = Number(block.dataset.activeSlide);
+  const indicators = block.querySelectorAll('.slide-indicator');
+  const totalSlides = [...indicators].length;
+  let moveTo = active + direction;
+  if (moveTo < 0) moveTo = (totalSlides - 1);
+  if (moveTo >= totalSlides) moveTo = 0;
+
+  block.querySelector('.slide-indicator.selected').classList.remove('selected');
+  indicators[moveTo].classList.add('selected');
+  block.dataset.activeSlide = moveTo;
+  const toSlide = block.querySelector(`.carousel-slide:nth-child(${moveTo + 1})`);
+  block.scrollTo({ top: 0, left: toSlide.offsetLeft - toSlide.parentNode.offsetLeft, behavior: 'smooth' });
+}
 
 export default async function decorate(block) {
   const indicators = createElement('div', 'carousel-slide-indicators');
 
   [...block.children].forEach((row, i) => {
+    row.classList.add('carousel-slide');
     const classes = ['image', 'text'];
     classes.forEach((e, j) => {
       if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
@@ -18,16 +35,26 @@ export default async function decorate(block) {
   block.append(indicators);
 
   const buttons = createElement('div', 'carousel-buttons');
-  const buttonPrev = createElement('div', ['carousel-button', 'carousel-button-prev'], {
+  const buttonPrev = createElement('span', ['icon', 'icon-arrow-left', 'carousel-button', 'carousel-button-prev'], {
     'aria-label': 'Previous Slide',
     role: 'button',
   });
-  const buttonNext = createElement('div', ['carousel-button', 'carousel-button-next'], {
+  buttonPrev.addEventListener('click', () => {
+    changeSlide(-1, block);
+  });
+  const buttonNext = createElement('span', ['icon', 'icon-arrow-right', 'carousel-button', 'carousel-button-next'], {
     'aria-label': 'Next Slide',
     role: 'button',
   });
+  buttonNext.addEventListener('click', () => {
+    changeSlide(1, block);
+  });
+  window.addEventListener('resize', () => {
+    changeSlide(0, block);
+  });
   buttons.append(buttonPrev);
   buttons.append(buttonNext);
+  decorateIcons(buttons);
   block.append(buttons);
-  block.classList.add('appear');
+  block.dataset.activeSlide = '0';
 }
