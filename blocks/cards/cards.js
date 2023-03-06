7cad81dacb758b9929a94eba7e12f315932e83b9
Daniel Ho <hoqizheng@gmail.com>
2023-03-06 12:05:18 -0800

    News section (#35)
    Add news card section
    
    ---------
    
    Co-authored-by: Daniel Ho <qho@adobe.com>

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,21 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 import { createElement } from '../../scripts/scripts.js';
 
+function addLinksToNewsCards(block) {
+  [...block.firstChild.children].forEach((card) => {
+    const cardBody = card.children[1];
+    const link = cardBody.querySelector('p');
+    const linkWrapper = createElement('a', 'card-link');
+    linkWrapper.href = link.textContent;
+    link.remove();
+    linkWrapper.appendChild(card.children[0].cloneNode(true));
+    linkWrapper.appendChild(card.children[1].cloneNode(true));
+    card.children[1].remove();
+    card.children[0].remove();
+    card.appendChild(linkWrapper);
+  });
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = createElement('ul');
@@ -16,4 +31,9 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+  /* add links for news cards */
+  if (block.classList.contains('news')) {
+    addLinksToNewsCards(block);
+  }
 }
