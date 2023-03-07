5ee18e88e7a556257f9d1974ff4a74f2246667f3
Sean Steimer <ssteimer@adobe.com>
2023-03-07 06:02:20 -0800

    embed block and ability to use an embed inside a cols

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,10 +1,25 @@
-export default function decorate(block) {
+import { buildBlock, decorateBlock, loadBlock } from '../../scripts/lib-franklin.js';
+
+export default async function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
+  const embedBlocks = [];
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
+      // setup embed
+      const link = col.querySelector('a');
+      if (link && link.href === link.textContent) {
+        const linkWrapper = link.closest('div');
+        if (linkWrapper && linkWrapper.children.length === 1) {
+          const embedBlock = buildBlock('embed', [['Source', link]]);
+          linkWrapper.append(embedBlock);
+          decorateBlock(embedBlock);
+          embedBlocks.push(embedBlock);
+        }
+      }
+
+      // setup image columns
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
@@ -15,4 +30,10 @@ export default function decorate(block) {
       }
     });
   });
+
+  for (let i=0; i <= embedBlocks.length; i += 1) {
+    const eb = embedBlocks[i];
+    // eslint-disable-next-line no-await-in-loop
+    await loadBlock(eb);
+  }
 }
