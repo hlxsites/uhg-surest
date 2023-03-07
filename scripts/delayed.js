7ce1ca2973535ed017be2bee78bbd22be8d39d58
Sean Steimer <ssteimer@adobe.com>
2023-03-07 12:31:33 -0800

    chore: gtm and segment

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,28 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { fetchPlaceholders, sampleRUM } from './lib-franklin.js';
+
+const placeholders = await fetchPlaceholders();
+
+function loadGoogleTagManager() {
+  // google tag manager
+  const { gtmId } = placeholders;
+  // eslint-disable-next-line
+  (function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', gtmId);
+}
+
+function loadSegment() {
+  const segmentKey = placeholders.segmentId;
+  /* eslint-disable */
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="YOUR_WRITE_KEY";analytics.SNIPPET_VERSION="4.15.2";
+  analytics.load(segmentKey);
+  analytics.page();
+  }}();
+  /* eslint-enable */
+}
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+loadGoogleTagManager();
+loadSegment();
