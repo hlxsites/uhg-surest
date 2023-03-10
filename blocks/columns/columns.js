8b1e167514ad2cf6f931054a74f08d2b9f537a5c
Sean Steimer <ssteimer@adobe.com>
2023-03-09 21:42:01 -0800

    support for bg images. refactor for the split hightlightsand more options for columns for embed/video/fragment

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,29 +4,50 @@ export default async function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  if (block.classList.contains('split-highlight')) {
+    block.closest('.section').classList.add('split-highlight-columns-container');
+  }
+
   const subBlocks = [];
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       // setup embed
       const link = col.querySelector('a');
-      const linkWrapper = link?.closest('div');
-      if (link && link.href === link.textContent) {
+      if (link) {
+        const linkWrapper = link?.closest('div');
         if (linkWrapper && linkWrapper.children.length === 1) {
-          const embedBlock = buildBlock('Embed', [['Source', link]]);
-          linkWrapper.append(embedBlock);
-          linkWrapper.classList.add('columns-img-col');
-          decorateBlock(embedBlock);
-          subBlocks.push(embedBlock);
-        }
-      } else if (link) {
-        const linkUrl = new URL(link.href);
-        const fragmentDomains = ['localhost', 'www.surest.com', '/main--uhg-surest--hlxsites.hlx.page', '/main--uhg-surest--hlxsites.hlx.live'];
-        if (link.textContent.endsWith(linkUrl.pathname)
-          && fragmentDomains.includes(linkUrl.hostname)) {
-          const fragmentBlock = buildBlock('fragment', [['Source', link]]);
-          linkWrapper.append(fragmentBlock);
-          decorateBlock(fragmentBlock);
-          subBlocks.push(fragmentBlock);
+          const linkUrl = new URL(link.href);
+          let linkTextUrl;
+          try {
+            linkTextUrl = new URL(link.textContent);
+          } catch {
+            // not a url, ignore
+          }
+          if (linkTextUrl && linkTextUrl.pathname === linkUrl.pathname) {
+            const fragmentDomains = ['localhost', 'www.surest.com', '/main--uhg-surest--hlxsites.hlx.page', '/main--uhg-surest--hlxsites.hlx.live'];
+            if (fragmentDomains.includes(linkUrl.hostname)) {
+              // fragment or video
+              if (linkUrl.pathname.includes('.mp4')) {
+                // video
+                const videoBlock = buildBlock('video', [['Source', link]]);
+                linkWrapper.append(videoBlock);
+                decorateBlock(videoBlock);
+                subBlocks.push(videoBlock);
+              } else {
+                // fragment
+                const fragmentBlock = buildBlock('fragment', [['Source', link]]);
+                linkWrapper.append(fragmentBlock);
+                decorateBlock(fragmentBlock);
+                subBlocks.push(fragmentBlock);
+              }
+            } else {
+              const embedBlock = buildBlock('Embed', [['Source', link]]);
+              linkWrapper.append(embedBlock);
+              linkWrapper.classList.add('columns-img-col');
+              decorateBlock(embedBlock);
+              subBlocks.push(embedBlock);
+            }
+          }
         }
       }
 
