ee8ad85a19242af37e481e05232eb39c0cbe61bc
Brendan Robert <brobert@adobe.com>
2023-03-10 13:18:15 -0600

    Main and QA styles of accordion done

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,12 +4,13 @@ export default function decorate(block) {
   const sections = createElement('div');
   [...block.children].forEach((row) => {
     const section = createElement('details');
+    const details = createElement('div');
+    details.classList.add('details');
     const sectionSummary = createElement('summary');
-    const summary = row.children[0];
-    const details = row.children[1];
-    [...summary.children].forEach((child) => sectionSummary.append(child));
+    sectionSummary.innerHTML = row.children[0].innerHTML;
+    details.innerHTML = row.children[1].innerHTML;
     section.append(sectionSummary);
-    [...details.children].forEach((child) => section.append(child));
+    section.append(details);
     sections.append(section);
     row.remove();
   });
