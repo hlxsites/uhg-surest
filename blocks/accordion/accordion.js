96a5641f9b43eb18bd9b8db00b6d5a66cd39f33f
Brendan Robert <brobert@adobe.com>
2023-03-10 13:19:46 -0600

    Small simplification

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,11 +4,10 @@ export default function decorate(block) {
   const sections = createElement('div');
   [...block.children].forEach((row) => {
     const section = createElement('details');
-    const details = createElement('div');
+    const details = row.children[1];
     details.classList.add('details');
     const sectionSummary = createElement('summary');
     sectionSummary.innerHTML = row.children[0].innerHTML;
-    details.innerHTML = row.children[1].innerHTML;
     section.append(sectionSummary);
     section.append(details);
     sections.append(section);
